89b4d6b3b1fddf66da95e8b5c508cdfcdbb859a8
Dave Y <daveyoon64@gmail.com>
2021-08-18 23:54:47 -0700

    experimenting with typescript's interface

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ function sayHi(humanName) {
     alert("Hello, " + humanName + "!");
 }
 sayHi("Bill Bellichek");
+;
+function greet(person) {
+    alert("Hello, " + person.firstName);
+}
+function greetLouder(person) {
+    alert("HELLO, " + person.firstName + "!!!!");
+}
+var person = { firstName: "Frank", hairColor: "Black" };
+greet(person);
+greetLouder(person);
--- app.ts
+++ app.ts
@@ -2,3 +2,20 @@ function sayHi(humanName: string) {
     alert(`Hello, ${humanName}!`);
 }
 sayHi("Bill Bellichek");
+
+interface IPerson {
+  firstName: string;
+};
+
+function greet(person: IPerson) {
+  alert(`Hello, ${person.firstName}`);
+}
+
+function greetLouder(person: IPerson) {
+  alert(`HELLO, ${person.firstName}!!!!`);
+}
+
+const person = {firstName : "Frank", hairColor: "Black" };
+
+greet(person);
+greetLouder(person);
